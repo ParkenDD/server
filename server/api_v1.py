2bdd9bb0273020f2e10e88a06fc2d4886b90fddb
Kilian Koeltzsch <me@kilian.io>
2019-08-15 13:47:59 +0200

    add more placeholder routes

--- server/api_v1.py
+++ server/api_v1.py
@@ -1,8 +1,30 @@
 from flask import Blueprint
 
-v1 = Blueprint('v1', __name__)
+v1 = Blueprint('v1', 'v1')
 
 
-@v1.route('/hello')
-def hello_world():
-    return 'Hello, World!'
+@v1.route('/')
+def meta():
+    return {
+        'api_version': '1.0',
+        'cities': {},
+        'reference': '',
+        'server_version': ''
+    }
+
+
+@v1.route('/<city_name>')
+def city(city_name):
+    return {
+        'last_downloaded': '',
+        'last_updated': '',
+        'lots': []
+    }
+
+
+@v1.route('/<city_name>/<lot_id>/timespan')
+def timespan(city_name, lot_id):
+    return {
+        'version': 1.0,
+        'data': {}
+    }
--- server/api_v2.py
+++ server/api_v2.py
@@ -1,8 +1,6 @@
 from flask import Blueprint
 
-from .db.db import *
-
-v2 = Blueprint('v2', __name__)
+v2 = Blueprint('v2', 'v2')
 
 
 @v2.route('/pools')
@@ -18,4 +16,11 @@ def pool(pool_name):
     return {
         'pool': pool_name,
         'lots': []
+    }
+
+
+@v2.route('/timespan/<lot_id>')
+def timespan(lot_id):
+    return {
+        'data': []
     }
